fe4818f5ebc30f682b5f6c58c8b53863d23adc78
Sam Fuksa <sam@samfuksa.eu>
2025-05-30 10:29:44 +0200

    opravena chyba s vyskou

--- bmi_new.bat
+++ bmi_new.bat
@@ -2,10 +2,10 @@
 
 REM nacteni udaju
 set /p vaha=zadej vahu v kg:
-set /p vyska=zadej vysku v m:
+set /p vyska=zadej vysku v cm:
 
 REM vypocet BMI
-set /a bmi=%vaha%/(%vyska% * %vyska%)
+set /a bmi=%vaha% * 10000 /(%vyska% * %vyska%)
 
 REM vypis BMI
 echo pro vahu: %vaha% a vysku: %vyska% je BMI: %bmi%
