02128b106b3f1a9691f7031cc3704e9aa867c746
Sam Fuksa <sam@samfuksa.eu>
2025-05-30 11:15:32 +0200

    pridano ukladani do txt a dotazovani se na jmeno

--- bmi_new.bat
+++ bmi_new.bat
@@ -1,16 +1,22 @@
 @echo off
 
+:start
+
 REM nacteni udaju
+set /p jmeno=zadej sve jmeno:
 set /p vaha=zadej vahu v kg:
 set /p vyska=zadej vysku v cm:
+
 
 REM vypocet BMI
 set /a bmi=%vaha% * 10000 /(%vyska% * %vyska%)
 
 REM vypis BMI
-echo pro vahu: %vaha% a vysku: %vyska% je BMI: %bmi%
+echo pro %jmeno% s vahou: %vaha% a vyskou: %vyska% je BMI: %bmi%
 
 
+echo pro %jmeno% s vahou: %vaha% a vyskou: %vyska% je BMI: %bmi% >> bmi.txt
 
 
+goto start
 pause
